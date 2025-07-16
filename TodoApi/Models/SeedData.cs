1d1123b49a6baaebe27eb699f05c1da7fc41aa1b
Andenis.Bukauskas <Andenis.Bukauskas@AAD16018>
2025-07-16 11:39:41 +0300

    Add project files.

---
+++ TodoApi/Models/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace TodoApi.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new TodoContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<TodoContext>>()))
+        {
+            // Look for any movies.
+            if (context.TodoItems.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.TodoItems.AddRange(
+                new TodoItem
+                {
+                    Title = "Wash dishes",
+                    DueDate = DateOnly.Parse("2025-7-13"),
+                    IsCompleted = true,
+                },
+                new TodoItem
+                {
+                    Title = "Takeout trash",
+                    DueDate = DateOnly.Parse("2025-7-14"),
+                    IsCompleted = true,
+                },
+                new TodoItem
+                {
+                    Title = "Clean bathroom",
+                    DueDate = DateOnly.Parse("2025-7-17"),
+                    IsCompleted = false,
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public DateOnly DueDate { get; set; }
+    public bool IsCompleted {  get; set; }
+}
